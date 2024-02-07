e432440461f61d0e1d78154db8938cd693283e2c
victormusembi11 <victor.musembi11@gmail.com>
2024-02-08 02:24:55 +0300

    renamed home page file & moved to pages folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./pages/home";
 import reportWebVitals from "./reportWebVitals";
 import Login from "./pages/login";
 import Dashboard from "./pages/dashboard";
--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { axiosInstanceAuth } from "./axios/axios";
+import { axiosInstanceAuth } from "../axios/axios";
 
 function App() {
   const [users, setUsers] = useState([]);
