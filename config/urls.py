68900da4fd4defedace40126092ba851169e68c0
Richie Mugambi <richiemugambi4@gmail.com>
2024-02-07 13:24:00 +0300

    feat: added paths for api/login/

--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework_simplejwt.views import TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("drf_social_oauth2.urls", namespace="drf")),
     path("api/", include("core.urls")),
+    path("api/login/", include("rest_social_auth.urls_jwt_pair")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_`obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
