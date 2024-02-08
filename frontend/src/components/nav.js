41c0ede67fb4dea5b56e36cf7bcea6875b245e52
victormusembi11 <victor.musembi11@gmail.com>
2024-02-08 22:59:11 +0300

    dynamic nav links

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -1,13 +1,46 @@
+import { useState, useEffect } from "react";
+
 export default function Nav() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) setToken(token);
+
+    return () => {
+      setToken(null);
+    };
+  }, [token]);
+
   return (
     <nav>
       <ul>
         <li>
           <a href="/">Home</a>
         </li>
-        <li>
-          <a href="/login">Login</a>
-        </li>
+        {token ? (
+          <>
+            <li>
+              <a href="/dashboard">Dashboard</a>
+            </li>
+            <li>
+              <button
+                onClick={() => {
+                  localStorage.removeItem("token");
+                  console.log("Token removed: ", token);
+                  setToken(null);
+                }}
+              >
+                Logout
+              </button>
+            </li>
+          </>
+        ) : (
+          <li>
+            <a href="/login">Login</a>
+          </li>
+        )}
       </ul>
     </nav>
   );
