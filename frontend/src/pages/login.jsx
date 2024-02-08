eeaba474c2e3d86c88a481d68a46b917aa562b2c
victormusembi11 <victor.musembi11@gmail.com>
2024-02-08 21:49:55 +0300

    clean up

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,8 +1,4 @@
 import { useState } from "react";
-import { GoogleLogin } from "@react-oauth/google";
-
-import { HandleGoogleLogin } from "../axios/google-login";
-import { axiosInstance } from "../axios/axios";
 
 export default function Login() {
   const [credentials, setCredentials] = useState({
@@ -19,34 +15,11 @@ export default function Login() {
     }));
   }
 
-  async function handleSubmit(e) {
+  function handleSubmit(e) {
     e.preventDefault();
 
-    try {
-      const response = await axiosInstance.post("/auth/token/", {
-        username: credentials.username,
-        password: credentials.password,
-        grant_type: "password",
-        client_id:
-          "541785145737-n1a0n0e914ipg2ug6oliop4ro19f78q2.apps.googleusercontent.com",
-        client_secret: "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",
-      });
-      console.log(response.data);
-      localStorage.setItem("access_token", response.data.access_token);
-      localStorage.setItem("refresh_token", response.data.refresh_token);
-    } catch (error) {
-      console.log(error);
-    }
+    console.log(credentials);
   }
-
-  const responseMessage = (response) => {
-    console.log(response);
-    HandleGoogleLogin(response.credential);
-  };
-
-  const errorMessage = (error) => {
-    console.log(error);
-  };
 
   return (
     <form>
@@ -55,8 +28,7 @@ export default function Login() {
       <button type="submit" onClick={handleSubmit}>
         submit
       </button>
-      <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-      <a href="https://www.facebook.com/v19.0/dialog/oauth?client_id=906353464279347&redirect_uri=http://localhost:3002/&state={facebook}&response_type=token">
+      <a href="https://www.facebook.com/v19.0/dialog/oauth?client_id=906353464279347&redirect_uri=http://localhost:3000/dashboard&state={facebook}&response_type=token">
         Login with Facebook
       </a>
     </form>
